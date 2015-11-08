6cca90ae93aecc83f8c70a4560e0895c8070324d
mmatheus07 <matheus.oliveira@ctrl365.com.br>
2015-11-08 17:38:07 -0200

    Part IX

--- js/script.js
+++ js/script.js
@@ -1,3 +1,59 @@
 $(document).ready(function(){
   $('.bxslider').bxSlider();
 });
+
+function mudaImagem(i) {
+
+	if (i == 1) {
+
+      document.getElementById("imagem1").src="img/anchor3.png";
+
+    } else if (i == 1) {
+
+    	document.getElementById("imagem2").src="img/anchor3.png";
+
+    }
+
+}
+
+function mudaImagem2(i) {
+
+	if (i == 1) {
+
+        document.getElementById("imagem1").src="img/anchor1.png";
+
+    } else if (i == 1) {
+
+    	document.getElementById("imagem2").src="img/anchor3.png";
+
+    }
+
+}
+
+function mudaImagem3(i) {
+
+	if (i == 1) {
+
+      document.getElementById("imagem3").src="img/anchor2.png";
+
+    } else if (i == 1) {
+
+    	document.getElementById("imagem4").src="img/anchor2.png";
+
+    }
+
+}
+
+function mudaImagem4(i) {
+
+	if (i == 1) {
+
+        document.getElementById("imagem3").src="img/anchor4.png";
+
+    } else if (i == 1) {
+
+    	document.getElementById("imagem4").src="img/anchor2.png";
+
+    }
+
+}
